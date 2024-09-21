8a6b695cf37522f493f3de1620e1c70bca303118
xapresser <115385708+xapresser@users.noreply.github.com>
2024-09-21 14:05:31 -0400

    files create

---
+++ backend/src/main/java/com/citizensmqp/backend/services/msgService.java
@@ -0,0 +1,23 @@
+package com.citizensmqp.backend.services;
+
+import com.citizensmqp.backend.models.userModel;
+import com.citizensmqp.backend.repositorys.userRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class msgService {
+    private final msgRepository msgRepo;
+
+    public void saveMsg(msgModel msg){
+        return msgRepo.save(msg);
+    }
+
+    public void getMsgs(){
+        msgRepo.findAll();
+    }
+}
