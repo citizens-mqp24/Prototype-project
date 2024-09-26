3d9953df054c956db71bff51226070adfab971f9
Gabriel Shiu <gabriel@shiu.us>
2024-09-26 13:38:04 -0400

    post to get oauth token

--- backend/src/main/java/com/citizensmqp/backend/controller/authController.java
+++ backend/src/main/java/com/citizensmqp/backend/controller/authController.java
@@ -5,10 +5,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,15 +21,18 @@ public class authController {
     @Value("${OAUTH2_CLIENT_ID}")
     String clientId;
 
+    @Value("${OAUTH2_CLIENT_SECRET}")
+    String clientSecret;
+
     @Value("${OAUTH2_REDIRECT_URI}")
     String redirectUri;
 
     String scope = "openid profile email";
+    String tokenUri = "https://oauth2.googleapis.com/token";
+    String grantType = "authorization_code";
 
-    // Endpoint to trigger the OAuth2 authorization request
     @GetMapping()
     public void oauthLogin(HttpServletResponse response) throws IOException {
-        System.out.println("redirect uri: " + redirectUri);
         String authorizationEndpoint = "https://accounts.google.com/o/oauth2/auth";
         String authorizationUrl = authorizationEndpoint
                 + "?response_type=code"
@@ -34,14 +40,26 @@ public class authController {
                 + "&redirect_uri=" + URLEncoder.encode(redirectUri, StandardCharsets.UTF_8)
                 + "&scope=" + URLEncoder.encode(scope, StandardCharsets.UTF_8);
 
-        // Redirect to OAuth2 provider's authorization endpoint
         response.sendRedirect(authorizationUrl);
     }
 
-    // Callback URL to capture authorization code
     @GetMapping("/callback")
-    public String oauthCallback(@RequestParam("code") String code) {
-        System.out.println("Authorization Code: " + code);
-        return "authorization-code-received";
+    public String handleOAuth2Callback(@RequestParam(name = "code") String authorizationCode) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, String> tokenRequest = new HashMap<>();
+        tokenRequest.put("code", authorizationCode);
+        tokenRequest.put("client_id", clientId);
+        tokenRequest.put("client_secret", clientSecret);
+        tokenRequest.put("redirect_uri", redirectUri);
+        tokenRequest.put("grant_type", grantType);
+
+        Map<String, String> tokenResponse = restTemplate.postForObject(tokenUri, tokenRequest, Map.class);
+
+        assert tokenResponse != null;
+        String accessToken = tokenResponse.get("access_token");
+        System.out.println("access token: " + accessToken);
+
+        return "access-token-received!!!";
     }
 }
